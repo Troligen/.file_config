401843e5203fce4768d0648ae23b7d79d2d5b118
troligen <utvecklarjesper@gmail.com>
2024-10-14 14:47:49 +0200

    Added a more custom theme to lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,9 +6,16 @@ return {
 		"nvim-lualine/lualine.nvim",
 		config = function()
 			-- I will later create a custom theme for lualine
-			-- local custom_gruvbox = require'lualine.themes.gruvbox'
+			local custom_gruvbox = require("lualine.themes.gruvbox_dark")
+			custom_gruvbox.insert.a.bg = "#A1B3B9"
+			custom_gruvbox.visual.a.bg = "#B98888"
+			custom_gruvbox.command.a.bg = "#DEB896"
+			custom_gruvbox.normal.c.bg = "#262626"
+			custom_gruvbox.insert.c.bg = "#262626"
+			custom_gruvbox.visual.c.bg = "#262626"
+			custom_gruvbox.command.c.bg = "#262626"
 			require("lualine").setup({
-				options = { theme = "gruvbox_dark" },
+				options = { theme = custom_gruvbox },
 				sections = { lualine_x = { "copilot", "encoding", "fileformat", "filetype" } },
 			})
 		end,
