bc602e4e176c2c4db392cf52466c518943d76bdd
troligen <utvecklarjesper@gmail.com>
2024-10-14 09:09:21 +0200

    added lsp and formater for java

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "ts_ls" },
+				ensure_installed = { "lua_ls", "pyright", "ts_ls", "jdtls" },
 			})
 		end,
 	},
@@ -19,7 +19,7 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
-			local servers = { "lua_ls", "pyright", "ts_ls" }
+			local servers = { "lua_ls", "pyright", "ts_ls", "jdtls" }
 			for _, lsp in ipairs(servers) do
 				lspconfig[lsp].setup({
 					capabilities = capabilities,
